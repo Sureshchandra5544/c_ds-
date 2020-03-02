e9346aacf33d7569a206ad3ea4bf8e8bebfafa28
srisaiphaneendra <srisaiphaneendra@gmail.com>
2020-03-02 15:44:45 +0530

    learning commit1

--- bubble_sort.c
+++ bubble_sort.c
@@ -4,7 +4,7 @@ int main()
    int count, temp, i, j, number[30];
    printf("Enter the number of elements for sorting:");
    scanf("%d",&count);
-   printf("Enter the numbers to be sorted %d :\n ",count);
+   printf("Enter the numbers to be sorted %d : ",count);
    for(i=0;i<count;i++)
    scanf("%d",&number[i]);
    for(i=count-2;i>=0;i--)
@@ -19,8 +19,9 @@ int main()
         }
       }
    }
-   printf("Sorted elements: ");
+   printf("Sorted elements are: ");
    for(i=0;i<count;i++)
-      printf(" %d",number[i]);
+   printf(" %d",number[i]);
+   printf("\n");
    return 0;
 }
